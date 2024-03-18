9cbeb5d606b65835723b68863ff5c69da1927ab3
AWESOMEACOUSTIC <adityapanigrahy98@gmail.com>
2024-03-18 11:54:46 +0530

    Update-2

--- Brohub_frontend/src/components/Main/Landing.jsx
+++ Brohub_frontend/src/components/Main/Landing.jsx
@@ -5,10 +5,11 @@ const Landing = () => {
         <div className='w-full h-screen bg-zinc-900 pt-1'>
             <div className="str mt-40 px-20">
                 {["Dive Into", "The World OF", "Your Community"].map((item, index) => {
-                    return <div className="masker overflow-hidden">
-                        <div className='w-fit flex items-center'>
-                            {index ===1 && (<div className='w-[vw]top-[-1vw] h-[5vw] bg-red-500'></div>)}
-                        <h1 className='uppercase text-9xl leading-[8vw] tracking-tighter font-mono'>{item}</h1>
+                    return <div className="masker">
+                        <div className='w-fit flex items-end '>
+                            {index ===1 && 
+                            (<div className='mr-5 w-[8vw] rounded-md h-[5vw] -top-[1.2vw] relative bg-red-500'></div>)}
+                        <h1 className='uppercase text-9xl leading-[8vw] tracking-normal font-mono'>{item}</h1>
                         </div>
                     </div>
 
@@ -22,8 +23,8 @@ const Landing = () => {
                     {item}
                 </p>)}
                 <div className="start flex items-center gap-5">
-                    <div className='px-4 py-2 border-[2px] border-zinc-700 rounded-full'>Connect
-                    </div>
+                    <button className='px-4 py-2 border-[2px] border-zinc-700 rounded-full cursor-pointer'>Connect
+                    </button>
                     <div className='w-10 h-10 justify-center flex items-center border-[1px] border-zinc-700 rounded-full'>
                         <span className='rotate-[45deg]'>
                             <FaArrowUpLong />
